dc1399442def4820de35ed4d8803f61835dee61c
Theo <dave@peon>
2013-09-13 17:50:11 +0200

    inital commit

---
+++ js/minion.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	/**
+	 * Js event handler to change between Clark and Superman modes.
+	 *
+	 * @author Ezequiel Calvo <ezecafre@gmail.com> 
+	 *                        Follow me @EzequielCalvo
+	 *                        Hashtag #CSSDrawing #Minion
+	 */
+
+	$('.btn').on('click', function() {
+	  $('#target').toggleClass('superman');
+	  $(this).toggleClass('active');
+	});
+               });
